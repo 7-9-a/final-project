a0d33e5f39ac1da1337a29dc3603e75d3d6a9cbf
unknown <katielaborda@gmail.com>
2018-07-26 12:53:16 -0700

    created basic component skeletons and routing between them

---
+++ client/src/components/Login.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Login = () => (
+  <div>
+    <h2>Log In</h2>
+    <input type="text" placeholder="User Name" />
+    <br />
+    <input type="password" placeholder="Password" />
+    <br />
+    <button>Login</button>
+  </div>
+);
+
+export default Login;
---
+++ client/src/components/SignUp.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const SignUp = () => (
+  <div>
+    <h2>Sign Up</h2>
+    <input type="text" placeholder="Username" />
+    <br />
+    <input type="text" placeholder="Email" />
+    <br />
+    <input type="text" placeholder="Password" />
+    <br />
+    <input type="text" placeholder="Confirm Password" />
+    <br />
+    <button>Sign Up</button>
+  </div>
+);
+
+export default SignUp;
